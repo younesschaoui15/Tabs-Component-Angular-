10694b67a80eceeaa377daaf04c9b7a68b57de79
Youness CHAOUI <younesschaoui15@gmail.com>
2025-12-02 14:32:02 +0100

    Project init

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Tabs} from './components/tabs/tabs';
+import {Tab} from './components/tab/tab';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [
+    Tabs,
+    Tab
+  ],
+  styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('Tabs-Component');
 }
